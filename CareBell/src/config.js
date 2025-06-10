729929f1f9945479761f4438b4e5bfd0d76de5a4
Leo Feldman <leolight214@gmail.com>
2025-06-10 15:21:36 +0300

    [CareBell] updated api url to vercel

--- CareBell/src/config.js
+++ CareBell/src/config.js
@@ -1,2 +1,3 @@
-export const API = "https://belladb.leontari.uk";
+export const API = "https://carebelldb.vercel.app";
+//export const API = "https://belladb.leontari.uk";
 //export const API = "https://localhost:4443";
