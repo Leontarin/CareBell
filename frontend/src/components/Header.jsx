3ac1bcf23d27f4dfc7b85a6b8a3b7dc880512d92
Mor Hodaya Maman <Mor.Hodaya.Maman@e.braude.ac.il>
2025-12-13 21:29:56 +0200

    [Prod2] Removed Emergency button for testing

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -148,9 +148,7 @@ export default function Header() {
           >
             ⚙️
           </button>
-          <button className="bg-red-500 text-white py-2 px-4 rounded-xl dark:bg-red-600">
-            {t("Header.Emergency")}
-          </button>
+          
         </div>
       </header>
 
